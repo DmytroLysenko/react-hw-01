bbdd93200fb7db3f761214772f12f201e8ccd858
DmytroLysenko <DmytroLysenko@users.noreply.github.com>
2020-09-24 15:02:08 +0300

    Updates

---
+++ precache-manifest.eda998d481881e3df238dcaf9f4b9908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4263daa4bbcdf423bcd109f40596a0",
+    "url": "/react-hw-01/index.html"
+  },
+  {
+    "revision": "1455ba3352a1966f1723",
+    "url": "/react-hw-01/static/css/main.72fb3d0f.chunk.css"
+  },
+  {
+    "revision": "d1788a6022a3ecb6015e",
+    "url": "/react-hw-01/static/js/2.47907ff3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-hw-01/static/js/2.47907ff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1455ba3352a1966f1723",
+    "url": "/react-hw-01/static/js/main.2ac68e0c.chunk.js"
+  },
+  {
+    "revision": "d989dce81fc3a5595a19",
+    "url": "/react-hw-01/static/js/runtime-main.e5f5ddc5.js"
+  }
+]);
